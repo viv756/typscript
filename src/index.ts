699b5d03aeed694295a272217ec10ec4845b8751
*vivek <vrvivekofficial@gmail.com>
2025-01-09 15:20:59 +0530

    functions

--- src/index.ts
+++ src/index.ts
@@ -22,21 +22,30 @@
 
 // type
 
-type Deatails = {
-  name: string;
-  // union operator
-  age: number | string;
-  salary: number;
-  getName: () => void;
-};
+// type Deatails = {
+//   name: string;
+//   // union operator
+//   age: number | string;
+//   salary: number;
+//   getName: () => void;
+// };
 
-let userDetails: Deatails = {
-  name: "vivek",
-  age: "twenty two",
-  salary: 100,
-  getName() {
-    console.log(this.name);
-  },
-};
+// let userDetails: Deatails = {
+//   name: "vivek",
+//   age: "twenty two",
+//   salary: 100,
+//   getName() {
+//     console.log(this.name);
+//   },
+// };
 
-let skills : (string| number)[] = ["js","ts",12,34]
+// let skills : (string| number)[] = ["js","ts",12,34]
+
+
+
+
+function getUsername({name,age} :{name:string, age:number}) {
+  return age
+}
+
+getUsername({name:"vivek",age:22})
