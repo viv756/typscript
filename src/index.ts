f9b4b7bb5f7f32136e54e501c81269e0ea079be9
*vivek <vrvivekofficial@gmail.com>
2025-01-09 15:05:23 +0530

    type keyword

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,30 @@
 
 // Definibg types
 
-interface Deatails {
+// interface Deatails {
+//   name: string;
+//   age: number;
+//   salary: number;
+//   getName: () => void;
+// }
+
+// let userDetails: Deatails = {
+//   name: "vivek",
+//   age: 22,
+//   salary: 100,
+//   getName() {
+//     console.log(this.name);
+//   },
+// };
+
+// type
+
+type Deatails = {
   name: string;
   age: number;
   salary: number;
   getName: () => void;
-}
+};
 
 let userDetails: Deatails = {
   name: "vivek",
