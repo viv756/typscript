3eb6297cd5b051503afeb974a9fa316a6d6377dc
*vivek <vrvivekofficial@gmail.com>
2025-01-09 14:55:40 +0530

    types in typescript

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,14 @@
 "use strict";
-const a = 344;
-alert(a);
+// infer types
+// let userName = "vivek"
+// Definibg types
+let userName = "vivek";
+let number = 34;
+let boolean = true;
+let skills = ["j", "ts"];
+let num = [1, 2, 3, 4];
+let userDetails = {
+    name: "vivek",
+    age: 22,
+    salary: 100,
+};
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,17 @@
-const a = 344
-alert(a)
+// infer types
+
+// let userName = "vivek"
+
+// Definibg types
+
+let userName: string = "vivek";
+let number: number = 34;
+let boolean: boolean = true;
+let skills: string[] = ["j", "ts"];
+let num: number[] = [1, 2, 3, 4];
+
+let userDetails: { name: string; age: number; salary: number } = {
+  name: "vivek",
+  age: 22,
+  salary: 100,
+};
